8f1ce39fbca11bc952d0a57a84301cae546b4c55
Jessenaps <jessegerrit@gmail.com>
2021-04-13 22:45:58 +0200

    geupdate iets

--- src/main/resources/ScriptsR/digitVis.R
+++ src/main/resources/ScriptsR/digitVis.R
@@ -1,7 +1,6 @@
 # Koen Molenaar
-
 library(RPostgres)
-con<-dbConnect(RPostgres::Postgres(), dbname="IMDB", host="localhost", port=5432, user="postgres",password="password")
+con<-dbConnect(RPostgres::Postgres(), dbname="project2", host="localhost", port=5432, user="postgres",password="4616030")
 dbListTables(con)
 
 # Eerst laden we de data in een dataframe
@@ -10,41 +9,23 @@ data <- dbReadTable(con, "movies")
 titles <<- subset(data, select = c(title))
 
 # Dit zijn de functies die zo voor elk nummer en voor iedere regel in titles TRUE of FALSE teruggeeft
-table1 <- function(x){
-  return(grepl("one",tolower(x)) | grepl("1",x))
-}
+table1 <- function(x){ return(grepl("one",tolower(x)) | grepl("1",x)) }
 
-table2 <- function(x){
-  return(grepl("two",tolower(x)) | grepl("2",x))
-}
+table2 <- function(x){ return(grepl("two",tolower(x)) | grepl("2",x)) }
 
-table3 <- function(x){
-  return(grepl("three",tolower(x)) | grepl("3",x))
-}
+table3 <- function(x){ return(grepl("three",tolower(x)) | grepl("3",x)) }
 
-table4 <- function(x){
-  return(grepl("four",tolower(x)) | grepl("4",x))
-}
+table4 <- function(x){ return(grepl("four",tolower(x)) | grepl("4",x)) }
 
-table5 <- function(x){
-  return(grepl("five",tolower(x)) | grepl("5",x))
-}
+table5 <- function(x){ return(grepl("five",tolower(x)) | grepl("5",x)) }
 
-table6 <- function(x){
-  return(grepl("six",tolower(x)) | grepl("6",x))
-}
+table6 <- function(x){ return(grepl("six",tolower(x)) | grepl("6",x)) }
 
-table7 <- function(x){
-  return(grepl("seven",tolower(x)) | grepl("7",x))
-}
+table7 <- function(x){ return(grepl("seven",tolower(x)) | grepl("7",x)) }
 
-table8 <- function(x){
-  return(grepl("eight",tolower(x)) | grepl("8",x))
-}
+table8 <- function(x){ return(grepl("eight",tolower(x)) | grepl("8",x)) }
 
-table9 <- function(x) {
-  return(grepl("nine",tolower(x)) | grepl("9",x))
-}
+table9 <- function(x) { return(grepl("nine",tolower(x)) | grepl("9",x)) }
 
 # Voor de snelheid gebruiken we zo veel mogelijk concurrency
 library(parallel)
@@ -55,9 +36,7 @@ clust <- makeCluster(cores)
 list_of_functions <- c(table1, table2, table3, table4, table5, table6, table7, table8, table9)
 
 # Deze functie geeft het aantal regels met het getal erin verschillend per functie
-calculate <- function(table_number, titles) {
-  return(table(lapply(titles, table_number))[[2]])
-}
+calculate <- function(table_number, titles) { return(table(lapply(titles, table_number))[[2]]) }
 
 # Hier maken we de tabel met per nummer het aantal titles waar het nummer in voorkomt
 tables <- parLapply(list_of_functions, calculate, titles=titles, cl=clust)
@@ -77,15 +56,14 @@ benford <- c(0.301, 0.176, 0.125, 0.097, 0.079, 0.067, 0.058, 0.051, 0.046)
 
 # Uiterlijk barplot
 library(RColorBrewer)
-coul <- brewer.pal(5, "Set2")
+coul <- brewer.pal(5, "Set2") 
 
 # We slaan de tabel op in een png bestand
-png(filename="C:/Users/molen/OneDrive - NHL Stenden/Documents/Bigdata-nieuw/src/main/resources/static/images/digitVis.png")
+png(filename="src/main/resources/static/images/digitVis.png")
 df.bar <- barplot(df[,1], main="Number counts in IMDB titles", xlab = "number", ylab="count", col = coul, ylim = c(0, 40000))
 lines(x = df.bar, y = sum(df[,1]) * benford, lwd = 3, col = "black")
 points(x = df.bar, y = sum(df[,1]) * benford)
-legend("topleft", legend=c("Wet van Benford"),
-       col=c("black"), lty=1:2, cex=0.8)
+legend("topleft", legend=c("Wet van Benford"), col=c("black"), lty=1:2, cex=0.8)
 dev.off()
 
 # De wet van Benford zegt dat de getallen 1-9 in een dataset niet evenvaak voorkomen (11,1%) maar eerder dat de eerste getallen vaker voorkomen dan de getallen erna.
